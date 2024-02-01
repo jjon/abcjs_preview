9b4a2948434f30c7b98be0915ece3de1f7b9b578
Jon Crump <crumpjjon@gmail.com>
2024-01-31 16:33:18 -0800

    Update abcjs_preview.mjs
    clean up opening comment

--- abcjs_preview.mjs
+++ abcjs_preview.mjs
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
-// This is a BBEdit text filter for transforming `abc` music notation into a
-// block of `SVG` that renders standard music engraving so that BBEdit may
-// display a preview. To use it you have to have `nodejs` present on your
-// system, and you have to install Daniel Narey's `scoped` ES module
-// `abc-render-svg` like this: `npm i @folkdb/abc-render-svg`. That module, in
-// turn relies on Paul Rosen's `abcjs` module, and the `domjs` module. All that
-// is present in `@folkdb/abc-render-svg`.
-
-// This ES module will not support `require()` so the BBEdit script needs the
-// file extension `.mjs` in order to import the module. That `.mjs` file needs
-// to live at `~/Library/Appliation Support/BBEdit/Text Filters/`. Use it by
-// selecting some ABC text, and invoking the Text Filter. The selected text will
-// be replaced by an SVG element which BBEdit can preview as if it were in an
-// HTML page.
+// This is a BBEdit text filter for transforming abc music notation into a block
+// of SVG that renders standard music engraving so that BBEdit may display a
+// preview. To use it you have to have nodejs present on your system, and you
+// have to install Daniel Narey's scoped ES module abc-render-svg like this: npm
+// install @folkdb/abc-render-svg. That module, in turn relies on Paul Rosen's
+// abcjs module, and the jsdom module. Everything you need is in
+// @folkdb/abc-render-svg.
+//
+// This ES module will not support require() so the node script needs the file
+// extension .mjs in order to import the module. That .mjs file needs to live at
+// ~/Library/Application Support/BBEdit/Text Filters/abcjs_preview.mjs. Use it
+// by selecting some ABC text, and invoking the abcjs_preview Text Filter. The
+// selected text will be replaced by an SVG element which BBEdit can preview.
 
 import abcRenderSvg from '@folkdb/abc-render-svg'
 
